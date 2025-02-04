bdc2c8ebae5efcf69946de0ecdb94459a9f8b263
camille-clappaz <camille.clappaz@laplateforme.io>
2025-02-04 11:51:29 +0100

    exo6

--- 06_composition.js
+++ 06_composition.js
@@ -8,7 +8,8 @@
  *  - for, foreach, while, do while sont interdits
  */
 
-const concat = (arr1, arr2) => {}
+const concat = (arr1, arr2) =>[...arr1,...arr2];
+console.log(concat([2,3,5],[7,8,9]))
 
 /**
  * utiliser l'opérateur de composition ... afin d'ajouter un élément à un tableau
@@ -19,7 +20,8 @@ const concat = (arr1, arr2) => {}
  * - ne pas utiliser la méthode push
  */
 
-const push = (arr, item) => {}
+const push = (arr, item) => [...arr, item]
+console.log(push([1,3,5], 7))
 
 /**
  * utiliser l'opérateur de composition ... afin de fusionner 2 objets passés en paramètres
@@ -27,7 +29,8 @@ const push = (arr, item) => {}
  * ex: {a: 1, b: 2}, {c: 3, d: 4} => {a: 1, b: 2, c: 3, d: 4}
  */
 
-const merge = (obj1, obj2) => {}
+const merge = (obj1, obj2) => ({ ...obj1, ...obj2 });
+console.log(merge({a: 1, b: 2}, {c: 3, d: 4}));
 
 /**
  * utiliser l'opérateur de composition ... afin de modifier la propriété name de l'objet
@@ -38,7 +41,8 @@ const merge = (obj1, obj2) => {}
  *  - interdiction d'utiliser l'opérateur d'affectation "="
  */
 
-const setName = (obj, name) => {}
+const setName = (obj, name) => ({ ...obj, name });
+console.log(setName({name: 'toto'}, 'titi'))
 
 // astuce: {...obj} crée une copie de l'objet, c'est un des principes de l'immutabilité et évite les problèmes de référence
 module.exports = {concat, push, merge, setName}
